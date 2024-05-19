f7e7920d172d7903c10ff529513af8c19c065269
siraj9949 <shaiksirajuddin9949@gmail.com>
2024-05-19 16:17:59 +0530

    strip method modified

--- src/stock_agent/components/data_ingestion.py
+++ src/stock_agent/components/data_ingestion.py
@@ -18,18 +18,19 @@ class DataIngestion:
     def download_new_data(self, interval = 30):
         uri_format = "https://api.twelvedata.com/time_series?apikey={api}&interval={interval}min&start_date={start_date}&end_date={end_date}&format=JSON&previous_close=true&symbol={symbol}"
         f = open(self.config.source_URL)
+        symbols = list(map(str.strip, symbols))
         symbols = f.readlines()
         ll,ul = self.get_interval(5)
         for i in range(len(symbols)):
-            uri = uri_format.format(api = os.environ["TWELVE_DATA_API"], interval = interval, start_date = ll, end_date = ul, symbol = symbols[i].strip())
+            uri = uri_format.format(api = os.environ["TWELVE_DATA_API"], interval = interval, start_date = ll, end_date = ul, symbol = symbols[i])
             response = requests.get(uri).json()
             if response["status"] == "error":
                 time.sleep(61)
                 response = requests.get(uri).json()
             data = response["values"]
             df = pd.DataFrame(data)
-            df.to_csv(self.config.local_data_file + f"{symbols[i].strip()}.csv")
-            logger.info(f">>>>>>>>>> {symbols[i].strip()}.csv saved to artifacts <<<<<<<<")
+            df.to_csv(self.config.local_data_file + f"{symbols[i]}.csv")
+            logger.info(f">>>>>>>>>> {symbols[i]}.csv saved to artifacts <<<<<<<<")
 
 
     def get_interval(self, length = 5):
